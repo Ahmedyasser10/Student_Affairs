a49a3f08bdf4d24460fbd18ec7d66f6ee5f64cc3
Mariam Esmat <100226656+mariamessmatt@users.noreply.github.com>
2023-04-13 17:33:37 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,23 +1,37 @@
-const username = document.getElementById("username");
-const password = document.getElementById("password");
-const form = document.querySelector("form");
-const pass1errorMessage = document.getElementById("pass1errorMessage");
-const pass2errorMessage = document.getElementById("pass2errorMessage");
-const usererrorMessage = document.getElementById("usererrorMessage");
+const form = document.getElementById('login');
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
 
-form.addEventListener("submit", (e) => {
 
-    if(username.value.trim() === ""){
-        e.preventDefault();
-        usererrorMessage.toggleAttribute('hidden');
-    }
-    if(password.value.length < 8){
-        e.preventDefault();
-        pass1errorMessage.toggleAttribute('hidden');
-    }
-    if(password.value.length > 15){
-        e.preventDefault();
-        pass2errorMessage.toggleAttribute('hidden');
-    }
+// Get the submit button
+const submitBtn = document.querySelector('#login-btn');
 
-})
+// Add an event listener to the submit button
+submitBtn.addEventListener('click', function (event) {
+  // Prevent the default form submission behavior
+  event.preventDefault();
+
+  if (username.value.trim() === '' || username.value.trim().length < 3) {
+    alert('Username should be more than 3 characters');
+    username.focus();
+    return false;
+  }
+  
+  if (password.value.trim() === ''|| password.value.trim().length <8) {
+    alert('Password should be at least 8 characters');
+    password.focus();
+    return false;
+  }
+
+
+const formData = {
+  'username': username.value,
+  'Password': password.value,
+};
+  console.log(formData);
+  console.log("Success");
+  
+  window.location.replace(
+  "navpage v2.html"
+);
+});
