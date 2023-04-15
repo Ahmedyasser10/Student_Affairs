fb68a43353de85b04d34be85ddfe617a13215cd5
Mariam Esmat <100226656+mariamessmatt@users.noreply.github.com>
2023-04-16 00:47:27 +0200

    Update login.js

--- login.js
+++ login.js
@@ -2,16 +2,22 @@ const form = document.getElementById('login');
 const username = document.querySelector("#username");
 const password = document.querySelector("#password");
 
-
 // Get the submit button
 const submitBtn = document.querySelector('#login-btn');
 
+const storedData = JSON.parse(localStorage.getItem('formData'));
+if (storedData) {
+  username.value = storedData['username'];
+  password.value = storedData['Password'];
+}
 // Add an event listener to the submit button
 submitBtn.addEventListener('click', function (event) {
   // Prevent the default form submission behavior
   event.preventDefault();
 
-  if (username.value.trim() === '' || username.value.trim().length < 3) {
+  let idRegex = /^\d{8}$/ ;
+
+  if (username.value.trim() === '' || username.value.trim().length < 3 || !idRegex.test(username.value)) {
     alert('Username should be more than 3 characters');
     username.focus();
     return false;
@@ -28,6 +34,7 @@ const formData = {
   'username': username.value,
   'Password': password.value,
 };
+  localStorage.setItem('formData',JSON.stringify(formData)); 
   console.log(formData);
   console.log("Success");
   
