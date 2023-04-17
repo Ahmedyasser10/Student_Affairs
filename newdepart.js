45202d9e1f8c67754a377f6e8707207ee9696715
Mariam Esmat <100226656+mariamessmatt@users.noreply.github.com>
2023-04-17 15:21:09 +0200

    Create newdepart.js

---
+++ newdepart.js
@@ -0,0 +1,56 @@
+const Fullname = document.getElementById("fullname");
+const ID = document.getElementById("id");
+const Level = document.getElementById("level");
+const departmentSelect = document.querySelector('select');
+const submitBtn = document.querySelector('#submit-btn');
+
+submitBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    let nameRegex = /^[a-zA-Z]+(?: [a-zA-Z]+)+$/;
+    let idRegex = /^\d{8}$/;
+
+    if (!idRegex.test(ID.value)) {
+        alert("Invalid ID");
+        return false;
+    }
+    if (!nameRegex.test(Fullname.value)) {
+        alert("invalid Name");
+        return false;
+    }
+    if ((Level.value <= 2) && (departmentSelect.value !== "General")) {
+        alert("this student can't be in a depatment");
+        return false;
+
+    }
+    if ((Level.value > 2) && (departmentSelect.value === "General")) {
+        alert("this student must have depatment");
+        return false;
+    }
+
+    let flag = false;
+    for (let i = 0; i < localStorage.length; i++) {
+        let k = localStorage.key(i);
+        let x = JSON.parse(localStorage.getItem(k));
+        if (x.type === 1) {
+            continue;
+        }
+        if (x.id === ID.value && x.name === Fullname.value && x.level === Level.value) {
+            flag = true;
+            x.depart = departmentSelect.value;
+            localStorage.setItem(k, JSON.stringify(x));
+            break;
+        }
+    }
+
+    if (!flag) {
+        console.log("no\n")
+        alert("This student is not found.");
+        return false;
+    }
+    else {
+        alert("Student's is Changed successfuly");
+    }
+
+
+});
