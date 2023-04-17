15d305982ce7c62b422d8ba9cc16f4cefbe3de7c
Mariam Esmat <100226656+mariamessmatt@users.noreply.github.com>
2023-04-17 21:18:42 +0200

    Update newdepart.js

--- newdepart.js
+++ newdepart.js
@@ -3,6 +3,8 @@ const ID = document.getElementById("id");
 const Level = document.getElementById("level");
 const departmentSelect = document.querySelector('select');
 const submitBtn = document.querySelector('#submit-btn');
+const logOut = document.getElementById("logout");
+const logOut2 = document.getElementById("logout2");
 
 submitBtn.addEventListener('click', function (event) {
     event.preventDefault();
@@ -49,8 +51,38 @@ submitBtn.addEventListener('click', function (event) {
         return false;
     }
     else {
-        alert("Student's is Changed successfuly");
+        alert("Student's department is Changed successfuly");
     }
 
 
 });
+
+logOut.onclick = function () {
+    for (i = 0; i < localStorage.length; i++) {
+        let k = localStorage.key(i);
+        if (JSON.parse(localStorage.getItem(k)).type === 0)
+            continue;
+
+        let x = JSON.parse(localStorage.getItem(k));
+        if (x.logedin === 1) {
+            x.logedin = 0;
+            localStorage.setItem(k, JSON.stringify(x));
+            break;
+        }
+    }
+}
+
+logOut2.onclick = function () {
+    for (i = 0; i < localStorage.length; i++) {
+        let k = localStorage.key(i);
+        if (JSON.parse(localStorage.getItem(k)).type === 0)
+            continue;
+
+        let x = JSON.parse(localStorage.getItem(k));
+        if (x.logedin === 1) {
+            x.logedin = 0;
+            localStorage.setItem(k, JSON.stringify(x));
+            break;
+        }
+    }
+}
