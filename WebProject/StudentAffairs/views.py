a46ece375aaf5d4f6af0dc7c9f56b28c0d093453
Ahemd Yasser <102255667+Ahmedyasser10@users.noreply.github.com>
2023-05-23 00:55:28 +0300

    Update views.py

--- WebProject/StudentAffairs/views.py
+++ WebProject/StudentAffairs/views.py
@@ -1,17 +1,50 @@
-from django.urls import path
-from . import views
+from django.template import loader
+from django.http import HttpResponse
+from .models import mem
+def Add(request):
+    newMem = mem.objects.get(id = 1)
+    context = {'mem' : newMem}
+    template = loader.get_template('Addstudent.html')
+    return HttpResponse(template.render(context,request))
 
-urlpatterns = [
-    path('Addstudent', views.Add, name='Add'),
-    path('delete', views.delete, name = 'delte'),
-    path('editstudent', views.edit, name = 'edit'),
-    path('login', views.login, name = 'login'),
-    path('navpagev2', views.nav, name = 'nav'),
-    path('user', views.user, name = 'user'),
-    path('NewDepartment', views.dep, name = 'dep'),
-    path('table', views.display, name = 'display'),
-    path('SignUp', views.signup, name = 'signup'),
-    path('', views.home, name = 'home'),
-    
-    
-]
+def delete(request):
+    template = loader.get_template('delete.html')
+    return HttpResponse(template.render())
+
+def edit(request):
+    template = loader.get_template('editstudent.html')
+    return HttpResponse(template.render())
+
+def login(request):
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render())
+
+
+def nav(request):
+    template = loader.get_template('navpagev2.html')
+    return HttpResponse(template.render())
+
+
+def user(request):
+    template = loader.get_template('user.html')
+    return HttpResponse(template.render())
+
+
+def dep(request):
+    template = loader.get_template('NewDepartment.html')
+    return HttpResponse(template.render())
+
+
+def display(request):
+    template = loader.get_template('table.html')
+    return HttpResponse(template.render())
+
+
+def signup(request):
+    template = loader.get_template('SignUp.html')
+    return HttpResponse(template.render())
+
+
+def home(request):
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render())
